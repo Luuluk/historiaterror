4351abbacab21687b0699bd868240638cef6ff23
Your Name <you@example.com>
2022-09-15 08:30:18 -0300

    primer version de historiaterror

---
+++ js/codigo.js
@@ -0,0 +1,116 @@
+let nombre = prompt ('Hola, soy Miguel, gracias por venir a escuchar mi historia. ¿Como te llamas?')
+
+alert ("Hola, un placer conocerte "+ nombre)
+
+alert("Te voy a contar algo que me pasó hace muchos años. Actualmente tengo 40 años. Esto me pasó cuando tenía 20. Es un recuerdo que me cambió mucho, a mi y a mi amigo Lucas. Hace 5 años salí del hospital psiquiatrico, estaba bajo control médico. Hoy sigo tomando las medicaciones, sin ellas creo que no podría estar tranquilo ahora.")
+
+
+alert ("Lucas y yo, como te dije teniamos 20 años, eramos mochileros que veníamos desde Bahía Blanca para conocer los pequeños pueblos que se encuentran en la provincia de Buenos Aires. Sin imaginar, que en uno de esos pequeños pueblos nos cambiaría completamente para siempre")
+
+alert ("Sin imaginar que por ese viaje terminariamos bajo control psiquitrico ")
+
+alert("Comenzemos...")
+
+alert ("El sol se escondía, eran las 19:00hs. Nosotros llegabamos a un pueblo casi abandonado, llamado Victorino de la Plaza. Recorrimos la estación de tren, los escombros de las pocas casas que quedaban de pie. No encontramos a nadie")
+
+alert ("A lo lejos pudimos ver luces...era otro pueblo, pensamos que era lo mejor irse para ese pueblo,ya que había más luces...se veía más habitable.")
+
+alert ("Recuerdo decirle a Lucas: Che Luquitas, y si nos vamos para allá, esto es un desierto, pasamos la noche allá y volvemos temprano para recorrer más tranquilos")
+
+alert ("Lucas no dijo nada, estaba pensativo, pero en realidad se había quedado en shock cuando vió una sombra a lo lejos y sonidos como si fueran gruñidos.")
+
+alert ("La sombra se acercaba a nosotros. Estabamos quietos, agarrados de las manos, sudando y con las palpitaciones super aceleradas. A medida que se acerba tomaba la forma de una persona")
+
+alert("El ambiente era tenso, era una noche fría, viento, se podían escuchar buhos, sonidos extraños. Nuestras mentes ya estaban distorcionando formas y sonidos.")
+
+alert ("El hombre se nos acercaba, y noté que traía una libreta en la mano. Seguíamos temblando del miedo. El señor era canoso, jorobado, estatura baja, y caminaba cojeando.")
+
+alert ("Hola, chicos, ¿están perdidos? No deberían estar acá.Vengan a mi casa, seguro deben tener frio y hambre. Siganme yo los voy a resguardar esta noche.- Nos dijo el viejo")
+
+alert ("Nosotros no confiabamos en el, pero el hambre y el frío hizo que accedieramos a ir con el viejo")
+
+alert ("Llegamos a la casa, un aspecto viejo. Era una especie de ´monoambiente´. Era una casa vieja, de madera, ya el techo se caía, el piso rechinaba, tenía una estufa a leña, y el hombre se estaba haciendo una sopa en una caldera. Mugre y mal olor, aún lo recuerdo.")
+
+alert ("Hola! Un gusto conocerlos. Mi nombre es Ernesto, tengo 90 años. Viví mucho tiempo en Victorino de la Plaza. Victorino se destacaba por el tren, eso nos mantenía vivos. Eramos como 1000 habitantes. Los únicos habitantes eramos mi esposa y yo. Ella falleció hace dos meses. ")
+
+alert ("Sientensen, les voy a servir un poco de sopa. Pongansen cómodos, no van a salir afuera hasta que el sol vuelva a aparecer. Es peligroso, y ustedes no conocen el terreno")
+
+alert ("Yo asustado le pregunto- ¿Porqué es peligroso? ¿Que pasa? ¿Hay animales peligrosos afuera? ¿Jabalí?¿Tigres?¿Leones?¿Cododrilos?¿Que pasa?-")
+
+alert ("El viejo me responde en un tono agitado y preocupado: Muchas preguntas, hay poco tiempo. Coman rápido, en 15 minutos quiero que se tapen todo el cuerpo con esta cobija, y se vayan a esconder dentro de mi armario. POR NADA, ¿me escucharon bien?, POR NADA SALGAN DEL ARMARIO, Y MUCHO MENOS SE DESTAPEN. ESTO LOS VA A SALVAR. Yo les diré cuando salir.")
+
+alert ("Lucas se animó a hablar, hasta que en un momento me dijo susurrando: Esto todo es una mentira Miguel, vayamosno, este viejo de mierda nos quiere generar miedo, mirá si es un pervertido, o un secuestrador, mirá si es un asesino.")
+
+alert ("Le respondo también susurrando: tranquilo Luqui, hagamosle caso, de último lo agarramos entre los dos, está en las últimas, hagamosle caso por las dudas, viste como se puso")
+
+alert ("Luego de pasados los 15 minutos...El viejo nos obliga a ponernos la cobija y a escondernos en el armario. Vimos como él agarró la libreta y se escondió en un baul, de esos viejos.")
+
+alert ("Habian pasado unos 30 minutos, y pensamos que era joda, afuera no escuchábamos nada, sólo el viento, Lucas me dijo que se iba a destapar la cabeza para ver por entre medio de la puerta. Yo le dije que no, que esperemos la orden del viejo para salir. No me hizo caso.")
+
+alert ("Lucas se destapó, y pude sentir como se sobresalto de miedo y al mismo tiempo se volvió a tapar la cabeza, estaba asustado, llorando. Algo que vió afuera del armario lo asustó...")
+
+alert("Hubo un silencio de 5 minutos, me dijo con voz quebradiza y susurrando lo más bajito que podía")
+
+alert ("Mi..miguel...de..de..debemos ir..irnos. Vi el baul...de...de..des..dess..")
+
+alert("Amigo tranquilo, le dije, contame que paso, que viste, hay tigres, jabali dentro de la casa? - Pero él me dijo...")
+
+alert("El viejo nos estaba mirando por la hendija de la puerta del armario y desapareció...se fue corriendo después. Nos estaba espiando. Me asusté al ver sus ojos. Ojos negros, todo Miguel, el ojo todo negro. Tengo miedo. Vayamosno a la mierda. Corramos para la ruta.")
+
+alert("Lucas volvió a mirar y me tocó el hombro como dandome una señal para que salgamos. Así hicimos. Salimos del armario. Cuando salimos ví arriba de la mesa un revolver con sus balas, ni lo dudé... lo agarré y salimos corriendo junto A Lucas, que también había agarrado una cuchilla.")
+
+alert ("Afuera nos encontramos con el viejo, leyendo en voz alta, casi gritando, su libreta en un idioma medio raro. Su libreta cubría su cara. Sus manos cuando nos vió salir empezaron a temblar, y se llevó una mano a la cabeza.")
+
+alert ("Recuerdo que nos dijo gritando...´¿NO LES DIJE QUE SE QUEDARAN TAPADOS Y NO SALIERAN?´")
+
+alert("Parecía una película, porque de repente comenzó a llover, muchisimo, caían truenos, se levanto mucho viento, yo lo apunté con el revolver. El viejo seguia gritando lo que estaba escrito en el libro, pensé que estaba rezando, nosé, no logro acordarme lo que decía.")
+
+alert ("Lucas, enojado, con miedo, actuó impulsivamente e intentó avalanzarse sobre el viejo. Pude agarrarlo, porque sentí que algo le iba a pasar si iba, escuchaba ruidos que se iban acercando.")
+
+alert ("Los pasos ahora parecían como de un caballo... ")
+
+alert ("Esa figura...pasó por arriba del hombre, pensé que lo había atropellado... pero no...")
+
+alert("Sentí un frio por atras... me dí vuelta... era el viejo con esos ojos negros, detrás había otra persona...era una mujer, con los mismos ojos negros... nos golpearon con un palo en la cabeza.")
+
+alert("Me desperté, en el mismo lugar donde vimos al viejo. Me desperté y al costado estaba Lucas llorando, con unos oficiales de policía. Escuché como Lucas le contaba a los policias lo que había pasado y como ellos se reían de él.")
+
+alert ("Uno dijo: estuvo buena la noche eh... vayan a su casa borrachos...va, si no consumieron otra cosa.")
+
+alert ("Lucas seguía llorando y explicando, me señaló a mi, me dijo que me escucharan a mi también, que nosotros teníamos razon. Me levante medio aturdido, y le dije que me acompañaran hasta donde estaba la casa donde el viejo nos había llevado")
+
+alert ("Cuando llegamos...no había casa. Sólo quedaban escombros.Había un caballo atado pero nos dijeron que eran los gauchos que hacían prueba de rienda")
+
+alert ("Se nos rieron en la cara...")
+
+alert ("Lucas y yo, alterados por la situación, empezamos a gritarle a la policía, que lo que deciamos era verdad. Nos dijeron que eso estaba abandonado desde hacía 10 años...")
+
+alert ("Que no vivía nadie...")
+
+alert ("Llegó la doctora y enfermeras a atendernos, ambos teníamos raspones en diferentes partes del cuerpo, pero ningún ´chichon´ en la cabeza. Lo cuál nos desconcertó porque Lucas me decía también que lo únicó que se acordaba era el golpe que le había dado el viejo")
+
+alert ("Nos derivaron a un médico psiquiatrico, porque seguían sin creernos, ellos pensaron que nosotros estábamos borrachos. Pero no, tanto Lucas como yo, vimos lo mismo...fue raro.")
+
+alert ("Al día de hoy, y luego de 20 años, no supe que pasó, no encuentro una explicación sobre ese día, porqué nos paso eso a nosotros? Del miedo que tuvimos pensamos eso? Paso en realidad? Tengo muchas preguntas sobre ese día")
+
+alert ("A Lucas lo voy a visitar al hospital todos los días, él no volvió a ser el mismo...el sigue viendo al viejo, caminando por la calle o en sus sueños. Sigue medicado, me contó que tiene ganas de terminar ese sufrimiento...yo lo veo muy mal, esa noche nos cambió para siempre.")
+
+
+alert ("Ahora "+ nombre + "...")
+
+alert ("¿me podes pasar el frasco de pastillas?")
+
+alert("creo que estoy viendo atrás tuyo al viejo")
+
+alert ("...hubo un silencio")
+
+alert("Decido salir del lugar, me despido de Miguel")
+
+alert ("(Luego de salir del lugar, le cuento la historia a mi amigo Elias)")
+
+let respuesta = prompt ('¿Que crees que paso Elias? ¿Que pensas de todo esto?')
+
+alert ("Si... aunque yo pienso que el viejo de verdad existe, se esconde para asustar a los que intentan aproximarse al lugar, pero no sé para que fin..., será porque tiene miedo que le roben sus pertenencias?... Nosé, creo que deberíamos escuchar tambíen la versíon de Lucas...¿No?")
+
+
+  
